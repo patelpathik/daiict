c01be5a2d35352908477530f39a4d0b8ce1eacb7
Pathik Patel <patelpathik1995@gmail.com>
2018-10-06 15:28:58 +0530

    Revert "Merge branch 'master' of https://github.com/patelpathik/daiict"
    This reverts commit 848f99254e520026df738a6e1906a29c801e4044, reversing
    changes made to 20632297e3d43cbd513fb2b17214b61c7c220e9e.

--- login.php
+++ login.php
@@ -1,51 +1,50 @@
 <?php
-	include "top.php";
+    include "top.php";
 ?>
-	<!-- ##### Breadcumb Area Start ##### -->
-	<div class="breadcumb-area bg-img" style="background-image: url(img/bg-img/breadcumb.jpg);">
-		<div class="bradcumbContent">
-			<h2>Login</h2>
-		</div>
-	</div>
-	<!-- ##### Breadcumb Area End ##### -->
+    <!-- ##### Breadcumb Area Start ##### -->
+    <div class="breadcumb-area bg-img" style="background-image: url(img/bg-img/breadcumb.jpg);">
+        <div class="bradcumbContent">
+            <h2>Login</h2>
+        </div>
+    </div>
+    <!-- ##### Breadcumb Area End ##### -->
 
-	<!-- ##### Google Maps ##### -->
+    <!-- ##### Google Maps ##### -->
    <br><br><br><br><br><br>
 
-	<!-- ##### Contact Area Start ##### -->
-	<section class="contact-area mt-100 section-padding-100-70">
-		<div class="container">
-			<div class="row">
-				<div class="col-12">
-					<div class="contact-content">
-						<div class="row">
-							<!-- Contact Information -->
-							
-							<!-- Contact Form Area -->
-							<div class="col-12 col-lg-3 "></div>
-							<div class="col-12 col-lg-6 ">
-								<div class="contact-form-area wow fadeInUp" data-wow-delay="500ms">
-									<form action="#" method="post">
-										<input type="email" class="form-control" id="uname" placeholder="User Name">
-										<input type="password" class="form-control" id="pass" placeholder="Password">
-										<center>
-											<button class="btn academy-btn mt-30" type="submit">Login</button>
-											<br><br>
-											<!-- <a class="nav-link active" id="tab--2" data-toggle="tab" href="#tab2" role="tab" aria-controls="tab2" aria-selected="false">Forgot Password?</a> -->
-											<a onclick="f_pass();" class="nav-link active">Forgot Password?</a>
-										</center>
-									</form> 
-								</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!-- ##### Contact Area End ##### -->
+    <!-- ##### Contact Area Start ##### -->
+    <section class="contact-area mt-100 section-padding-100-70">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="contact-content">
+                        <div class="row">
+                            <!-- Contact Information -->
+                            
+                            <!-- Contact Form Area -->
+                            <div class="col-12 col-lg-3 "></div>
+                            <div class="col-12 col-lg-6 ">
+                                <div class="contact-form-area wow fadeInUp" data-wow-delay="500ms">
+                                    <form action="#" method="post">
+                                        <input type="email" class="form-control" id="uname" placeholder="User Name">
+                                        <input type="password" class="form-control" id="pass" placeholder="Password">
+                                        <center><button class="btn academy-btn mt-30" type="submit">Login</button></center>
+                                        <br>
+                                        <center><button class="btn academy-btn" type="submit"> Forgot Password</button></center>
+
+
+                                    </form> 
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ##### Contact Area End ##### -->
 
 <?php
-	include "foot.php";
+    include "foot.php";
 ?>
--- top.php
+++ top.php
@@ -2,88 +2,89 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="description" content="">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-	<!-- Title -->
-	<title>Edcuation</title>
+    <!-- Title -->
+    <title>Edcuation</title>
 
-	<!-- Favicon -->
-	<link rel="icon" href="img/core-img/favicon.ico">
+    <!-- Favicon -->
+    <link rel="icon" href="img/core-img/favicon.ico">
 
-	<!-- Core Stylesheet -->
-	<link rel="stylesheet" href="style.css">
+    <!-- Core Stylesheet -->
+    <link rel="stylesheet" href="style.css">
 
 </head>
 
 <body>
-	<!-- ##### Preloader ##### -->
-	<div id="preloader">
-		<i class="circle-preloader"></i>
-	</div>
+    <!-- ##### Preloader ##### -->
+    <div id="preloader">
+        <i class="circle-preloader"></i>
+    </div>
 
-	<!-- ##### Header Area Start ##### -->
-	<header class="header-area">
+    <!-- ##### Header Area Start ##### -->
+    <header class="header-area">
 
-		<!-- Top Header Area -->
-		<div class="top-header">
-			<div class="container h-100">
-				<div class="row h-100">
-					<div class="col-12 h-100">
-						<div class="header-content h-100 d-flex align-items-center justify-content-between">
-							<div class="academy-logo">
-								<a href="index.html"><img src="img/core-img/logo.png" alt=""></a>
-							</div>
-						   
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+        <!-- Top Header Area -->
+        <div class="top-header">
+            <div class="container h-100">
+                <div class="row h-100">
+                    <div class="col-12 h-100">
+                        <div class="header-content h-100 d-flex align-items-center justify-content-between">
+                            <div class="academy-logo">
+                                <a href="index.html"><img src="img/core-img/logo.png" alt=""></a>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-		<!-- Navbar Area -->
-		<div class="academy-main-menu">
-			<div class="classy-nav-container breakpoint-off">
-				<div class="container">
-					<!-- Menu -->
-					<nav class="classy-navbar justify-content-between" id="academyNav">
+        <!-- Navbar Area -->
+        <div class="academy-main-menu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="academyNav">
 
-						<!-- Navbar Toggler -->
-						<div class="classy-navbar-toggler">
-							<span class="navbarToggler"><span></span><span></span><span></span></span>
-						</div>
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
 
-						<!-- Menu -->
-						<div class="classy-menu">
+                        <!-- Menu -->
+                        <div class="classy-menu">
 
-							<!-- close btn -->
-							<div class="classycloseIcon">
-								<div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
-							</div>
+                            <!-- close btn -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
 
-							<!-- Nav Start -->
-							<div class="classynav">
-								<ul>
-									<li><a href="index.html">Home</a></li>
-									<li><a href="volunteer/volunteer_signup.php">Volunteer Signup</a></li>
-									<li><a href="contact.php">Contact</a></li>
-									<li><a href="login.php">Login</a></li>
-								</ul>
-							</div>
-							<!-- Nav End -->
-						</div>
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="volunteer/volunteer_signup.php">Volunteer Signup</a></li>
+                                    <li><a href="contact.php">Contact</a></li>
+                                     <li><a href="login.php">Login</a></li>
 
-						<!-- Calling Info -->
-						<div class="calling-info">
-							<div class="call-center">
-								<a href="tel:+654563325568889"><i class="icon-telephone-2"></i> <span>(+91) 97897 89780</span></a>
-							</div>
-						</div>
-					</nav>
-				</div>
-			</div>
-		</div>
-	</header>
+                                </ul>
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+
+                        <!-- Calling Info -->
+                        <div class="calling-info">
+                            <div class="call-center">
+                                <a href="tel:+654563325568889"><i class="icon-telephone-2"></i> <span>(+91) 97897 89780</span></a>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
